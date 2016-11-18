f5958c13a9e48d164bf4e1600ec7aa4f151c0c01
James Ahlm <14782828428@qq.com>
2016-11-18 13:45:34 +0800

    Now there are links and nodes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,5 @@
 var url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json"
+var radius = 5
 
 var margins = {
 "top": 5,
@@ -21,12 +22,13 @@ d3.select("#chart")
 .attr("width", width + margins.left + margins.right)
 
 var draw = function(nodes, links){
-var force = d3.layout.force()
-.size([width, height])
-.nodes(nodes)
-.links(links)
 
-force.linkDistance(width/3)
+//Start Simulation
+var simulation = d3.forceSimulation(nodes)
+simulation.force("link", d3.forceLink(links))
+
+
+//Map Links and Nodes to SVG
 
 var svg = d3.select('#chart')
 
@@ -40,6 +42,24 @@ var node = svg.selectAll('.node')
 .enter().append('circle')
 .attr('class', 'node')
 
+//Paint on a turn of the simulation
+
+simulation.on('end', function() {
+
+node.attr('r', radius)
+.attr('cx', function(d) {return d.x})
+.attr('cy', function(d) {return d.y})
+
+//Source property?
+link.attr('x1', function(d) { return d.source.x; })
+.attr('y1', function(d) { return d.source.y; })
+.attr('x2', function(d) { return d.target.x; })
+.attr('y2', function(d) { return d.target.y; });
+
+})
+ 
+return simulation
+
 }
 
 
@@ -48,6 +68,8 @@ var node = svg.selectAll('.node')
 d3.json(url, function(data){
 
 var nodes = data.nodes
+console.log(nodes[4])
 var links = data.links
-
+console.log(links[3])
+draw(nodes, links)
 })
